28b9d9dce2cf6e15e202fd6d655a271433c41d9c
shr222 <shr222@lehigh.edu>
2023-01-23 13:37:42 -0500

    small fixes

--- TicTacToe/testgui.py
+++ TicTacToe/testgui.py
@@ -4,7 +4,7 @@ layout = [[pg.Text("Hello World")], [pg.Button("OK")]]
 testLayout = [[pg.Text("Hello World")]]
 window = pg.Window("Test", layout)
 
-pg.popup(custom_text= 'This is a pop up, welcome to my test program', background_color= "White") 
+pg.popup(title="Hello World", custom_text= 'This is a pop up, welcome to my test program', background_color= "White", auto_close_duration= 10) 
 
 while True:
     event, values = window.read()
